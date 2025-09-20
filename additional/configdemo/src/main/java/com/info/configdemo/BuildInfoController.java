3f2fd1de3c50ae77ccb564612241ed57dd5ac6d5
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-09-21 00:55:08 +0200

    environnement variables & Precedence Order & @configurationProperties

--- additional/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ additional/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -1,5 +1,6 @@
 package com.info.configdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,31 @@ public class BuildInfoController {
     @Value("${build.name}")
     private String buildName;
 
+
+    // use configurationProperties way
+    @Autowired
+    private BuildInfo buildInfo;
+
     @GetMapping("/build-info")
     public String getBuildInfo() {
-        return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
+//        return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
+        return buildInfo.toString();
     }
 }
+
+
+
+
+//Precedence Order
+//
+//→ Command-line arguments (--build.id=12345)
+//
+//→ Java system properties (-Dbuild.id=12345)
+//
+//→ OS environment variables (export BUILD_ID=12345)
+//
+//→ application.properties or application.yml
+//
+//→ Spring Cloud Config Server (if used)
+//
+//→ Default values inside the application code
