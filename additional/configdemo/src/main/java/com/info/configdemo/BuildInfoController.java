840c5d78cc5d35130e0568e6d41b66c6983524d9
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-09-21 00:08:06 +0200

    Spring Boot Profiles

---
+++ additional/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -0,0 +1,26 @@
+package com.info.configdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BuildInfoController {
+
+//    @Value("${OS:default}")
+    @Value("${build.id}")
+    private String buildId;
+
+//    @Value("${PROCESSOR_LEVEL:default}")
+    @Value("${build.version}")
+    private String buildVersion;
+
+//    @Value("${JAVA_HOME:default}")
+    @Value("${build.name}")
+    private String buildName;
+
+    @GetMapping("/build-info")
+    public String getBuildInfo() {
+        return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
+    }
+}
