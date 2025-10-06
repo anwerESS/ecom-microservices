95b020f630620abec850682630cf5ac811c36d26
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-10-07 00:09:54 +0200

    Implementing Circuit Breaking with  Fallback Mechanism

--- order/src/main/java/com/ecommerce/order/services/CartService.java
+++ order/src/main/java/com/ecommerce/order/services/CartService.java
@@ -7,6 +7,7 @@ import com.ecommerce.order.dtos.UserResponse;
 import com.ecommerce.order.repositories.CartItemRepository;
 import com.ecommerce.order.dtos.CartItemRequest;
 import com.ecommerce.order.models.CartItem;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ public class CartService {
     private final ProductServiceClient productServiceClient;
     private final UserServiceClient userServiceClient;
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "addToCartFallBack")
     public boolean addToCart(String userId, CartItemRequest request) {
         // Look for product
         ProductResponse productResponse = productServiceClient.getProductDetails(request.getProductId());
@@ -48,6 +50,13 @@ public class CartService {
            cartItemRepository.save(cartItem);
         }
         return true;
+    }
+
+    public boolean addToCartFallBack(String userId,
+                                     CartItemRequest request,
+                                     Exception exception) {
+        exception.printStackTrace();
+        return false;
     }
 
     public boolean deleteItemFromCart(String userId, String productId) {
