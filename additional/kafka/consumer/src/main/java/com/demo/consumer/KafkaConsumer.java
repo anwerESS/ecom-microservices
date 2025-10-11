07a4cb90028d29d7f0882f0282af9494ee7bd0f7
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-10-11 15:13:12 +0200

    Serializing and Deserializing Java Objects

--- additional/kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ additional/kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -16,5 +16,9 @@ public class KafkaConsumer {
         System.out.println("Received Message 2: " + message);
     }
 
-
+    @KafkaListener(topics = "my-topic-new", groupId = "my-new-group-rider")
+    public void listenRiderLocation(RiderLocation location) {
+        System.out.println("Received Location: " + location.getRiderId()
+                + " : " + location.getLatitude() + " : " + location.getLongitude());
+    }
 }
