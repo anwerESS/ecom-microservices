271e88b198851e994e5df6e60ca6517c681342e4
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-10-12 01:26:37 +0200

    RabbitMQ: Transitioning to Spring Cloud Streams and Functions

--- order/src/main/java/com/ecommerce/order/config/RabbitMQConfiguration.java
+++ order/src/main/java/com/ecommerce/order/config/RabbitMQConfiguration.java
@@ -1,67 +1,67 @@
-package com.ecommerce.order.config;
+// package com.ecommerce.order.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.amqp.core.*;
+// import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+// import org.springframework.amqp.rabbit.core.RabbitAdmin;
+// import org.springframework.amqp.rabbit.core.RabbitTemplate;
+// import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+// import org.springframework.amqp.support.converter.MessageConverter;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RabbitMQConfiguration {
-    @Value("${rabbitmq.queue.name}")
-    private String queueName;
+// @Configuration
+// public class RabbitMQConfiguration {
+//     @Value("${rabbitmq.queue.name}")
+//     private String queueName;
 
-    @Value("${rabbitmq.exchange.name}")
-    private String exchangeName;
+//     @Value("${rabbitmq.exchange.name}")
+//     private String exchangeName;
 
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
+//     @Value("${rabbitmq.routing.key}")
+//     private String routingKey;
 
-    @Bean
-    public Queue queue() {
-        return QueueBuilder.durable(queueName)
-                .build();
-    }
+//     @Bean
+//     public Queue queue() {
+//         return QueueBuilder.durable(queueName)
+//                 .build();
+//     }
 
-    @Bean
-    public TopicExchange exchange(){
-        return ExchangeBuilder.topicExchange(exchangeName)
-                .durable(true)
-                .build();
-    }
+//     @Bean
+//     public TopicExchange exchange(){
+//         return ExchangeBuilder.topicExchange(exchangeName)
+//                 .durable(true)
+//                 .build();
+//     }
 
-    @Bean
-    public Binding binding() {
-        return BindingBuilder.bind(queue())
-                .to(exchange())
-                .with(routingKey);
-    }
+//     @Bean
+//     public Binding binding() {
+//         return BindingBuilder.bind(queue())
+//                 .to(exchange())
+//                 .with(routingKey);
+//     }
 
-    /*
-    * This function creates an instance of AmqpAdmin, which is a Spring AMQP class responsible for declaring queues, exchanges and bindings on a RabbitMQ broker. It is used to create and manage RabbitMQ resources. The function takes a ConnectionFactory as a parameter, which is used to connect to the RabbitMQ broker. It configures the RabbitAdmin to automatically declare the queues, exchanges and bindings when the application starts up.
+//     /*
+//     * This function creates an instance of AmqpAdmin, which is a Spring AMQP class responsible for declaring queues, exchanges and bindings on a RabbitMQ broker. It is used to create and manage RabbitMQ resources. The function takes a ConnectionFactory as a parameter, which is used to connect to the RabbitMQ broker. It configures the RabbitAdmin to automatically declare the queues, exchanges and bindings when the application starts up.
 
-     * */
-    @Bean
-    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
-        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
-        admin.setAutoStartup(true);
-        return admin;
-    }
+//      * */
+//     @Bean
+//     public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+//         RabbitAdmin admin = new RabbitAdmin(connectionFactory);
+//         admin.setAutoStartup(true);
+//         return admin;
+//     }
 
-    @Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+//     @Bean
+//     public MessageConverter messageConverter() {
+//         return new Jackson2JsonMessageConverter();
+//     }
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(messageConverter());
-        template.setExchange(exchangeName);
-        return template;
-    }
-}
+//     @Bean
+//     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//         RabbitTemplate template = new RabbitTemplate(connectionFactory);
+//         template.setMessageConverter(messageConverter());
+//         template.setExchange(exchangeName);
+//         return template;
+//     }
+// }
